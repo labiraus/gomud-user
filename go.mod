2f945760a395538eb1be107b9b2598b92ff3b329
Oliver Hathaway <oliver.hathaway@bjss.com>
2021-04-08 22:34:23 +0100

    Initial create - rough and ready

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/labiraus/gomud-user
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+
+	"github.com/labiraus/gomud-common/api"
+)
+
+func main() {
+	fmt.Println("user starting")
+	ctx, ctxDone := context.WithCancel(context.Background())
+	done := api.StartBasicApi(ctx, userHandler)
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	s := <-c
+	ctxDone()
+	fmt.Println("hello got signal: " + s.String() + " now closing")
+	<-done
+}
+
+func userHandler(w http.ResponseWriter, r *http.Request) {
+	defer func() {
+		r := recover()
+		if r != nil {
+			log.Println(r)
+		}
+	}()
+
+	var request = api.UserRequest{}
+	err := api.UnmarshalRequest(&request, r)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	fmt.Printf("user handler got %#v\n", request)
+
+	response := api.UserResponse{Greeting: "from " + request.UserName}
+	fmt.Printf("user handler sending %#v\n", response)
+	api.MarshalResponse(response, w)
+}
