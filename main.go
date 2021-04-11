ae25f159d28b6a44668de12ee5329edee8361586
Oliver Hathaway <oliver.hathaway@bjss.com>
2021-04-11 19:49:05 +0100

    added request/response object so it'll build

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func userHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	}()
 
-	var request = api.UserRequest{}
+	var request = userRequest{}
 	err := api.UnmarshalRequest(&request, r)
 	if err != nil {
 		log.Println(err)
@@ -39,7 +39,25 @@ func userHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Printf("user handler got %#v\n", request)
 
-	response := api.UserResponse{Greeting: "from " + request.UserName}
+	response := userResponse{Greeting: "from " + request.UserName}
 	fmt.Printf("user handler sending %#v\n", response)
 	api.MarshalResponse(response, w)
+}
+
+type userRequest struct {
+	UserName string
+}
+
+type userResponse struct {
+	Greeting string
+}
+
+// Validate checks if it is a valid request
+func (r userRequest) Validate() error {
+	return nil
+}
+
+// Validate checks if it is a valid request
+func (r userResponse) Validate() error {
+	return nil
 }
